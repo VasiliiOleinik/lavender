76ec7de7bdd65efb307699ca82f638fd2025fff8
Vasilii <goooneoooogle@gmail.com>
2021-02-21 22:00:39 +0200

    [ADD]: карточка товара

--- app/js/common.js
+++ app/js/common.js
@@ -14,4 +14,19 @@ $(function () {
       },
     ]
   });
+
+  $('.product-carousel-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.product-carousel-nav'
+  });
+  $('.product-carousel-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.product-carousel-for',
+    dots: false,
+    focusOnSelect: true
+  });
 });
